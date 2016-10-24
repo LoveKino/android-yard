2264f0cb9f1296b5454d0e8ee619ff2a9854b061
loveKino <842914439@qq.com>
2016-10-24 17:50:06 +0800

    publish jar

--- install/index.js
+++ install/index.js
@@ -1,13 +1,18 @@
 'use strict';
 
 let spawnp = require('spawnp');
+let path = require('path');
 
 /**
  * @param yardDir
  *      eg: /data/user/0/com.android.freekite.patch.aosppatch
  */
 module.exports = (yardDir) => {
-    return spawnp('./deployYard.sh', [yardDir, '../target/yard-dex.jar'], {
-        cwd: __dirname
+    return spawnp('./deployYard.sh', [
+        yardDir,
+        path.join(__dirname, '../target/yard-dex.jar')
+    ], {
+        cwd: __dirname,
+        stdio: 'inherit'
     });
 };
